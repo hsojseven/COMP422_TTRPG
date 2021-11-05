1993d20e3e1b1688b83f45c9e72c98f2de53e183
Josh Smeltzer <smeltzerjs18@gcc.edu>
2021-11-04 21:42:56 -0400

    add db

--- server.py
+++ server.py
@@ -9,19 +9,39 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # set up DB
 scriptdir = os.path.dirname(os.path.abspath(__file__))
-dbfile = os.path.join(scriptdir, "ttrpg.db")
+dbfile = os.path.join(scriptdir, "ttrpg.sqlite3")
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbfile}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'Users'
-    name = db.Column(db.Unicode, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.Unicode, nullable=False)
+    password = db.Column(db.Unicode, nullable=False)
+    characters = db.relationship('Character', backref='Characters')
+
+class Game(db.Model):
+    __tablename__ = 'Games'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Unicode, nullable=False)
+    description = db.Column(db.Unicode, nullable=False)
+
+class Character(db.Model):
+    __tablename__ = 'Characters'
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userID=db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False) # FK
+    strength = db.Column(db.Integer, nullable=False)
+    dexterity = db.Column(db.Integer, nullable=False)
+    constitution = db.Column(db.Integer, nullable=False)
+    intelligence = db.Column(db.Integer, nullable=False)
+    wisdom = db.Column(db.Integer, nullable=False)
+    charisma = db.Column(db.Integer, nullable=False)
 
 # Use to clear tables and edit structure
 # WILL WIPE ALL DB DATA
-# db.drop_all()
-# db.create_all()
+db.drop_all()
+db.create_all()
 ##########################
 
 @app.route("/home/")
@@ -41,4 +61,4 @@ def login():
 		else:
 			for field,error in loginForm.errors.items():
 				flash(f"{field}: {error}")
-			return redirect(url_for("index"))
+			return redirect(url_for("login"))
