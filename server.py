9476cb57cd149d2ad524f23ec187c7da0b416a01
Njbeam <njbeam@gmail.com>
2021-11-16 20:17:46 -0500

    add character

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash, g, jsonify
-from myforms import GameForm, LoginForm, RegisterForm
+from myforms import GameForm, LoginForm, CharacterForm, RegisterForm 
 from flask_login import UserMixin, LoginManager, login_required
 from flask_login import login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
@@ -88,8 +88,8 @@ class Player(db.Model):
 
 # Use to clear tables and edit structure
 # WILL WIPE ALL DB DATA
-#db.drop_all()
-#db.create_all()
+db.drop_all()
+db.create_all()
 
 user1 = User(id=1, username="bobBuilder")
 user2 = User(id=2, username="laryLobster")
@@ -210,6 +210,22 @@ def addGame():
 @login_required
 def game():
 	return "<h2> This will be a game </h2>"
+
+
+@app.route("/characters", methods=["GET", "POST"])
+@login_required
+def get_characters():
+    newCharacterForm = CharacterForm()
+    if request.method == 'GET':
+        return render_template("characterForm.j2", characterList = db.session.query(Character).all(), user = current_user.id)
+    if request.method == "POST":
+        character = Character(id=current_user.id, name=newCharacterForm.name.data, strength=newCharacterForm.strength.data, dexterity=newCharacterForm.dexterity.data,
+        constitution=newCharacterForm.constitution.data, intelligence=newCharacterForm.intelligence.data, wisdom=newCharacterForm.wisdom.data, charisma=newCharacterForm.charisma.data)
+        db.sesssion.add(character)
+        db.session.flush()
+
+        return redirect(url_for("characters"))
+
 #-----------------------------------------------------------------------------------------
 #-------------------------------------- LOG OUT ------------------------------------------
 #-----------------------------------------------------------------------------------------
