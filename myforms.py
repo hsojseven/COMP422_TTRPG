da9d5a8fbf5f22f2a54e9d1d2ad2d27cb19e4d01
kratztr <57235857+kratztr@users.noreply.github.com>
2021-11-17 21:03:34 -0500

    added bootstraps to the add_character page

--- myforms.py
+++ myforms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, IntegerField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.fields.html5 import IntegerField
 from wtforms.fields.simple import TextAreaField
 from wtforms.validators import InputRequired, EqualTo, Length, NumberRange
 
@@ -24,11 +25,11 @@ class RegisterForm(FlaskForm):
   submit = SubmitField("Register")
 
 class CharacterForm(FlaskForm):
-  name = StringField("Enter Name: ", validators=[InputRequired()])
-  strength = IntegerField("Enter Strength: ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Please enter a number between 0 and 20.")])
-  dexterity = IntegerField("Enter Dexterity: ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Please enter a number between 0 and 20.")])
-  constitution = IntegerField("Enter Constitution: ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Please enter a number between 0 and 20.")])
-  intelligence = IntegerField("Enter Intelligence: ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Please enter a number between 0 and 20.")])
-  wisdom = IntegerField("Enter Wisdom: ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Please enter a number between 0 and 20.")])
-  charisma = IntegerField("Enter Charisma: ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Please enter a number between 0 and 20.")])
+  name = StringField("Character Name: ", validators=[InputRequired()])
+  strength = IntegerField("Strength (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Value must be between 0 and 20.")])
+  dexterity = IntegerField("Dexterity (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Value must be between 0 and 20.")])
+  constitution = IntegerField("Constitution (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Value must be between 0 and 20.")])
+  intelligence = IntegerField("Intelligence (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Value must be between 0 and 20.")])
+  wisdom = IntegerField("Wisdom (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Value must be between 0 and 20.")])
+  charisma = IntegerField("Charisma (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message="Value must be between 0 and 20.")])
   submit = SubmitField("Register")
