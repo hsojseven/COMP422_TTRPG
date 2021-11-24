505fd6e79d5f8b542c8cc42dbb4253d57db11aa2
kratztr <57235857+kratztr@users.noreply.github.com>
2021-11-24 09:21:50 -0500

    mainly bootstrap work like centering content and upgrading look of the forms

--- myforms.py
+++ myforms.py
@@ -6,31 +6,28 @@ from wtforms.validators import InputRequired, EqualTo, Length, NumberRange
 
 # define our own FlaskForm subclass for our form
 class LoginForm(FlaskForm):
-  username = StringField("Username: ", validators=[InputRequired()])
-  password = PasswordField("Password: ", 
-      validators=[InputRequired(), Length(min=8, max=256)])
-  submit = SubmitField("Submit")
-
+  username = StringField(validators=[InputRequired()])
+  password = PasswordField(validators=[InputRequired(), Length(min=8, max=256)])
+  submit = SubmitField("Login")
+  
 class GameForm(FlaskForm):
-  name = StringField("Name: ", validators=[InputRequired()])
-  description = TextAreaField("Description: ", validators=[InputRequired()])
-  submit = SubmitField("Submit")
+  name = StringField(validators=[InputRequired()])
+  description = TextAreaField(validators=[InputRequired()])
+  submit = SubmitField("Add")
 
 class RegisterForm(FlaskForm):
-  username = StringField("Enter Username: ", validators=[InputRequired()])
-  password = PasswordField("New Password: ", 
-      validators=[InputRequired(), Length(min=8, max=256)])
-  confirm_password = PasswordField("Confirm Password: ", 
-      validators=[EqualTo('password')])
+  username = StringField(validators=[InputRequired()])
+  password = PasswordField(validators=[InputRequired(), Length(min=8, max=256)])
+  confirm_password = PasswordField(validators=[EqualTo('password')])
   submit = SubmitField("Register")
 
 class CharacterForm(FlaskForm):
   integer_message = "Value must be between 0 and 20"
-  name = StringField("Character Name: ", validators=[InputRequired()])
-  strength = IntegerField("Strength (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
-  dexterity = IntegerField("Dexterity (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
-  constitution = IntegerField("Constitution (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
-  intelligence = IntegerField("Intelligence (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
-  wisdom = IntegerField("Wisdom (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
-  charisma = IntegerField("Charisma (between 0 and 20): ", validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
-  submit = SubmitField("Register")
+  name = StringField(validators=[InputRequired()])
+  strength = IntegerField(validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
+  dexterity = IntegerField(validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
+  constitution = IntegerField(validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
+  intelligence = IntegerField(validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
+  wisdom = IntegerField(validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
+  charisma = IntegerField(validators=[InputRequired(), NumberRange(min=0, max=20, message=integer_message)])
+  submit = SubmitField("Add")
