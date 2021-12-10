c350e685eed12c4bdd9bdd67acbb1fff19429911
Josh Smeltzer <57044982+hsojseven@users.noreply.github.com>
2021-12-09 22:51:06 -0500

    changes

--- static/editCharacter.js
+++ static/editCharacter.js
@@ -1,14 +1,24 @@
 window.addEventListener("DOMContentLoaded", function() {
-	
-	const editBtn = document.getElementById("editButton");
-	editBtn.addEventListener("click", pre_fill_form);
-    
+
+
+    document.getElementById('characterSubmit').addEventListener('click', function () {
+        postData('/api/editCharacter/' + 1 + '/', JSON.stringify())
+          .then(data => {
+            console.log("Posted to the server"); // JSON data parsed by `data.json()` call
+          });
+      });
 });
 
-
-// Fill character form fields with the previously saved details
-function pre_fill_form()
-{
-    
-    document.getElementById('').setAttribute('value', valueFromSQL);
-}
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'PUT',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      referrerPolicy: 'no-referrer', 
+      body: data 
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+    }
